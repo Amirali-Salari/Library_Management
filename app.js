99be00d8cd02d7d1e23f09b2969ff87d3654afb2
Amirali-Salari <amirali.salar@outlook.com>
2024-08-21 01:53:17 +0330

    Added routes to server and Bug fix. The router was added to the server and the problem of calling the controller in the router was solved. (First API completed.)

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
 import './configs/db.js'
+import usersRouter from './routes/usersRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 80;
+
+app.use('/api/users/', usersRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,5 +2,6 @@ import express from 'express';
 import * as usersController from '../controllers/usersController.js'
 
 const Router = express.Router();
+export default Router;
 
-Router.get('/', usersController.getAllUsers())
+Router.get('/', usersController.getAllUsers)
