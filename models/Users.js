1eb258e435f9de56403668fc6838b1a9a2386859
Amirali-Salari <amirali.salar@outlook.com>
2024-08-20 17:50:39 +0330

    Added 'Users' model.

---
+++ models/Users.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const usersSchema = new Schema({
+    name: String,
+    username: String,
+    email: String,
+    crime: Number,
+    role: String,
+});
+
+const users = model('users', usersSchema);
+export default users;
